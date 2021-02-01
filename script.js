ae8c4841b67f9d1bc49b3e096c5a1af1239ac0af
Pignataro67 <Pignataro67@gmail.com>
2021-02-01 05:06:28 -0800

    re-creating JavaScript file

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const addBtn = document.getElementById('add')
 
-const notes = JSON.parse(localStorage.getItem('notes'))
+addBtn.addEventListener('click', () => addnewNote())
 
-function updateLS() {
-  const notesText = document.querySelectorAll('textarea')
+functionAddNote (text = '') {
+  const note = document.createElement('div')
+  note.classList.add('note')
 
-  const notes = []
-
-  notesText.forEach(note => notes.push(note.value))
-
-  localStorage.setItem('notes', JSON.stringify(notes))
+  note.innerHTML = `
+  <div class="tools">
+    <button class="edit"><i class="fas fa-edit"></i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>
+  `
 }
 
 // const textEl = document.getElementById('text')
