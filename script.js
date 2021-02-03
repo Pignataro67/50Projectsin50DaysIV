fb89c294f4d98448f343eeb4da3b9f09d93a4650
Pignataro67 <Pignataro67@gmail.com>
2021-02-03 07:57:55 -0800

    adding JS for project

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
+const img = document.querySelectorAll('#imgs img')
+
+console.log(img)
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+  idx++
+  changeImage()
+}
+
+function changeImage() {
+  if(idx > img.length - 1) {
+      idx = 0
+  } else if(idx < 0) {
+      idx = img.length - 1
+  }
+
+  img.style.transform = `translateX(${-idx * 500}px)`
+}
 
 // localStorage.setItem('name', JSON.stringify())
 // JSON.parse(localStorage.getItem('name'))
