e84018a12f79a4cc7eca00968329fc1c0f34db73
Pignataro67 <Pignataro67@gmail.com>
2021-02-09 08:22:21 -0800

    adding HTML for next project

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
 
-listItems.forEach((item, idx) => {
-  item.addEventListener('click', () => {
-    hideAllContents()
-    hideAllItems()
 
-    item.classList.add('active')
-    contents[idx].classList.add('show')
-  })
-})
+// const contents = document.querySelectorAll('.content')
+// const listItems = document.querySelectorAll('nav ul li')
 
-function hideAllContents() {
-  contents.forEach(content => content.classList.remove('show'))
-}
+// listItems.forEach((item, idx) => {
+//   item.addEventListener('click', () => {
+//     hideAllContents()
+//     hideAllItems()
 
-function hideAllItems() {
-  listItems.forEach(item => item.classList.remove('active'))
-}
+//     item.classList.add('active')
+//     contents[idx].classList.add('show')
+//   })
+// })
+
+// function hideAllContents() {
+//   contents.forEach(content => content.classList.remove('show'))
+// }
+
+// function hideAllItems() {
+//   listItems.forEach(item => item.classList.remove('active'))
+// }
 
 // const container = document.getElementById('container')
 // const colors = ['#e74c3c', '8#44ad', '#3498db', '#e67e22', '#2ecc71']
