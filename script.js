b442f696ecd34e0ce33a29a1bedaaa56351814f7
Pignataro67 <Pignataro67@gmail.com>
2021-02-23 07:15:16 -0800

    adding HTML for next project

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const open_btn = document.querySelector('.open-btn')
-const close_btn = document.querySelector('.close-btn')
-const nav = document.querySelectorAll('.nav')
 
-open_btn.addEventListener('click', () => {
-  nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
-
-close_btn.addEventListener('click', () => {
-  nav.forEach(nav_el => nav_el.classList.remove('visible'))
-})
 
 // const boxesContainer = document.getElementById('boxes')
 // const btn = document.getElementById('btn')
