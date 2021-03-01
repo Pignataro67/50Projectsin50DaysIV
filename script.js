fb4448c101e97013182f23e1fd246811c4591a8d
Pignataro67 <Pignataro67@gmail.com>
2021-03-01 07:50:30 -0800

    debugging/spacing; form clear and marked/toggled completed added

--- script.js
+++ script.js
@@ -23,65 +23,19 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText
 
-    todosUl.appendChild()
-  }
-}
-
-function addTodo(todo) {
-  let todoText = input.value
-
-  if(todo) {
-    todoText = todo.text
-  }
-
-  if(todoText) {
-    const todoEl = document.createElement('li')
-  if(todo && todo.completed) {
-    todoEl.classList.add('completed')
-  }
-   
-  todoEl.innerText = todoText
-
-    todoEl.addEventListener('click', () => {
-    todoEl.classList.toggle('completed')
-      updateLS()
-    }) 
+    todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
 
     todoEl.addEventListener('contextmenu', (e) => {
       e.preventDefault()
 
       todoEl.remove()
-    updateLS()
-    }) 
+    })
 
-  todosUL.appendChild(todoEl)
+    todosUl.appendChild(todoEl)
 
-  input.value = ''
-
-    updateLS()
+    input.value = ''
   }
 }
-
-// const boxesContainer = document.getElementById('boxes')
-// const btn = document.getElementById('btn')
-
-// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
-
-// function createBoxes() {
-//   for (let i = 0; i < 4; i++) {
-//     for (let j = 0; j < 4; j++) {
-//       const box = document.createElement('div') 
-//       box.classList.add('box')
-//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-//       boxesContainer.appendChild(box)
-//     }
-//   }
-// }
-
-// createBoxes()
-
-
-
 
 // const contents = document.querySelectorAll('.content')
 // const listItems = document.querySelectorAll('nav ul li')
